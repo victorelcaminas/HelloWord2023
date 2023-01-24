01504480be24a5437d307569ae66b3c835e8380e
victor <victor@ieselcaminas.org>
2023-01-24 17:03:42 +0100

    initial commit

---
+++ src/main/java/org/ieselcaminas/helloworld/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.ieselcaminas.helloworld;
+
+/**
+ *
+ * @author victor
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String[] s = new String[2];
+        s[3] = "hhhh";
+    }
+}
